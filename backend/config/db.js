55f4c0985765d96cdab97aad20f8aa1291c55a43
JatinThakur022 <thakurjatinrajputana@gmail.com>
2024-03-15 12:36:37 -0400

    updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,18 @@
-const sequelize=require('sequelize')
-const dbInstance= new sequelize('project_test','root','toor',{
-host:'localhost',
-dialect:'mysql',
-operatorAliases:0,
-pool:{
-    max:5,
-    min:0,
-    acquire:30000,
-    idle:10000
-}
+const sequelize = require('sequelize')
+const configs = require('./config')
+const dbInstance = new sequelize(
+    configs.dbConfig.DB_NAME,
+    configs.dbConfig.USERNAME,
+    configs.dbConfig.PASSWORD, {
+    host: configs.dbConfig.HOST,
+    dialect: 'mysql',
+    operatorAliases: 0,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 })
 
-module.exports=dbInstance
+module.exports = dbInstance
