7a5b2a725eef8e82783332733514575b713fbc74
unknown <arjun.bhati1174@gmail.com>
2024-03-15 10:16:49 +0530

    updates by me

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CardPage, CategoryPage, CheckoutPage, DashboardPage, Productdeatilspage, SigninPage, SignupPage } from './Routes';
-import URL from './Routes/routes'
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path={URL._auth.signup} element={<SignupPage />} />
-          <Route path={URL._main.dashboard} element={< DashboardPage />} />
-          <Route path={URL._auth.signin} element={<SigninPage />} />
-          <Route path={URL._task.card} element={<CardPage />} />
-          <Route path={URL._task.category} element={<CategoryPage />} />
-          <Route path={URL._task.checkout} element={<CheckoutPage />} />
-          <Route path={URL._task.details} element={<Productdeatilspage />} />
-
-        </Routes>
-      </BrowserRouter>
-=======
 import './App.css';
 
 const App = () => {
@@ -56,7 +36,6 @@ const App = () => {
         <div className='box8'>Clothes</div>
         <div className='box9'>FootWear</div>
       </div>
->>>>>>> abd77ef52455b941f1a43b16f2238a0f38dd5949
     </div>
   )
 }
